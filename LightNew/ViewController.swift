2ea76b8ef12a4ddfc6035a0d8c867f4de1fbbeb8
Marchello <marchello.rnd@icloud.com>
2020-05-07 00:22:58 +0300

    Initial Commit

---
+++ LightNew/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LightNew
+//
+//  Created by мак on 07.05.2020.
+//  Copyright © 2020 Marchello. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
